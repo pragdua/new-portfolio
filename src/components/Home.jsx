51a0ec75a92c2682d5cdde8c9c24930bd663c0e3
pragdua <to.pragundua@gmail.com>
2021-12-08 01:59:32 +0530

    added live github stats

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyledHome } from '../styles/Home.styled'
 import SocialLinks from './SocialLinks'
 
+const endpoint = `https://api.github.com/users/pragdua/repos`;
+
+
 const Home = () => {
+
+    const [ghData, setGhData] = useState([]);
+
+    useEffect(() => {
+        fetch(endpoint)
+            .then(res => res.json())
+            .then((...data) => {
+
+                const info = data[0]
+                .sort(
+                    (a, b) => Number(new Date(b.updated_at)) - Number(new Date(a.updated_at))
+                )
+                .slice(0, 2);
+
+                setGhData(info);
+            });
+
+    },[])
+
+
+    console.log(ghData);
+
     return (
         <>
         <StyledHome>
@@ -11,7 +36,7 @@ const Home = () => {
         </div>
         <div className="desc">
         border-radius enthusiast. frontend x design. currently working on  
-        <span className="live-stats"> this data will be generated live.</span>
+        <span className="live-stats"> <a href={ghData[0].html_url}>{ghData[0].name}</a>, {ghData[0].description} and <a href={ghData[1].html_url}>{ghData[1].name}</a>, {ghData[1].description}.</span>
         </div>
         </StyledHome>
         <SocialLinks />
--- src/styles/Home.styled.js
+++ src/styles/Home.styled.js
@@ -4,11 +4,37 @@ const appear = keyframes`
 
 0%{
    opacity: 0;
-   transform: translateY(10px)  ;
+   transform: translateY(10px);
 }
 
 100%{
 
+}
+
+`;
+
+const flash = keyframes`
+
+0%{
+   color: white;
+}
+
+25%{
+   color: #D0FFEB;
+}
+
+50%{
+   color: #D0E3FF;
+
+}
+
+75%{
+   color: #FFB2B6;
+
+}
+
+100%{
+   color: white;
 }
 
 `;
@@ -19,18 +45,26 @@ export const StyledHome = styled.div`
    margin-top: 1.5rem;
    font-size: 1.2rem;
    animation: ${appear} 1s ease;
-
+   
    & .name {
       font-size: 1.7rem;
       font-weight: 600;
    }
-
+   
    & .desc {
       margin-top: 1rem;
+      color: #B5B5B5;
    } 
 
    & .live-stats {
-      color: grey;
+      /* color: #e5e5e5; */
+   }
+
+   & .live-stats a {
+      color: white;
+      text-decoration: none;
+      font-weight: bold;
+      animation: ${flash} 4s infinite;
    }
 
 
