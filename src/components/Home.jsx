8b4bf16b1627aa7904ee972ba89277ee7872c158
pragdua <to.pragundua@gmail.com>
2021-12-08 12:44:55 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,32 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { StyledHome } from '../styles/Home.styled'
 import SocialLinks from './SocialLinks'
 
-const endpoint = `https://api.github.com/users/pragdua/repos`;
-
 
 const Home = () => {
-
-    const [ghData, setGhData] = useState([]);
-
-    useEffect(() => {
-        fetch(endpoint)
-            .then(res => res.json())
-            .then((...data) => {
-
-                const info = data[0]
-                .sort(
-                    (a, b) => Number(new Date(b.updated_at)) - Number(new Date(a.updated_at))
-                )
-                .slice(0, 2);
-
-                setGhData(info);
-            });
-
-    },[])
-
-
-    console.log(ghData);
 
     return (
         <>
@@ -35,8 +12,8 @@ const Home = () => {
             pragun dua
         </div>
         <div className="desc">
-        border-radius enthusiast. frontend x design. currently working on  
-        <span className="live-stats"> <a href={ghData[0].html_url}>{ghData[0].name}</a>, {ghData[0].description} and <a href={ghData[1].html_url}>{ghData[1].name}</a>, {ghData[1].description}.</span>
+        border-radius enthusiast. frontend x design. community lead, twitter developer platform. 
+        currently working on <span className="live-stats"> <a href="https://twitter.com/pragdua">twittr.xyz</a></span>
         </div>
         </StyledHome>
         <SocialLinks />
